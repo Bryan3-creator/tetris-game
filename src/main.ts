517ce969be41224a307d83d69782fe5ca1a6a38b
Bryan.dev <bryxnbxy@gmail.com>
2023-12-21 08:35:47 -0500

    Added board consts and set on the canvas

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,18 @@
 import "./style.css";
+import {
+  BOARD_WIDTH,
+  BOARD_HEIGHT,
+  BLOCK_SIZE,
+  BOARD_BACKGROUND,
+} from "./consts";
+import { $ } from "./utils";
+
+const canvas = $<HTMLCanvasElement>({ name: "canvas" });
+const context = canvas.getContext("2d");
+
+if (context === null) throw new Error("Error canvas");
+
+canvas.width = BOARD_WIDTH * BLOCK_SIZE;
+canvas.height = BOARD_HEIGHT * BLOCK_SIZE;
+
+context.scale(BLOCK_SIZE, BLOCK_SIZE);
